996a13720fdf7faf35062a44977c535d137819ff
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-07 02:35:40 +0100

    diocane

--- script/scripts.js
+++ script/scripts.js
@@ -34,7 +34,7 @@ const refreshLista = function () {
 
     for (let i = 0; i < listaOnScreen.length; i++) {
 
-        let newTr = creatr(listaOnScreen[i].trPacchetto);
+        let newTr = creatr(listaOnScreen[i].titleTask);
         tBody.appendChild(newTr.trPacchetto)
     }
 
@@ -76,6 +76,14 @@ const creatr = function (riga) {
     pTrashIcon.classList = "trashIcon"
     pTrashIcon.onclick = function () {
         let riga = document.getElementById(prototrDaPushare.id);
+        for (let i = 0; i < listaOnScreen.length; i++) {
+            console.log("listaOnScreen[i].titleTask= [" + listaOnScreen[i].titleTask + "] prototrDaPushare.id= [" + prototrDaPushare.id + "]")
+            if ("idtr" + listaOnScreen[i].titleTask === prototrDaPushare.id) {
+                listaOnScreen.splice(i, 1)
+                console.log("Trovato")
+                console.log(listaOnScreen)
+            }
+        }
         riga.remove();
     };
     const icon = document.createElement("i")
@@ -91,6 +99,7 @@ const creatr = function (riga) {
     prototrDaPushare.appendChild(prototdTrashIcon)
 
     const pacchettoFinale = {
+        titleTask: riga,
         trPacchetto: prototrDaPushare,
         pPacchetto: "Aggiungi la tua descrizione del task!"
     }
@@ -99,23 +108,19 @@ const creatr = function (riga) {
 }
 
 
-function eliminatr(tr) {
-    var riga = document.getElementById(tr);
-    riga.remove();
-}
 
 const elemento1 = {
-    trPacchetto: "Formattazione Html",
+    titleTask: "Formattazione Html",
     pPacchetto: "Metto l'header del titolo, un main diviso tra to-do list e paragrafi descrittivi, un pulsante per salvare, checkbox e trashicon"
 }
 
 const elemento2 = {
-    trPacchetto: "Stile CSS",
+    titleTask: "Stile CSS",
     pPacchetto: "Palette, icone da fontawesome, font da google font e altri, flexbox, flexbox direction, margini, bordi"
 }
 
 const elemento3 = {
-    trPacchetto: "Codice JavaScript",
+    titleTask: "Codice JavaScript",
     pPacchetto: "Parto provando a scrivere degli script che creano righe uguali a quelle fatte dal file html. Se funziona, lo inserisco dinamicamente in un metodo che renderizza la pagina partire da un array di elementi. Aggiungo i listener per le funzioni previste"
 }
 
